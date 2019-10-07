68313d62092d814eb9e4468a8413d9158a8fc2de
Andrea Stagi <stagi.andrea@gmail.com>
2019-10-08 01:34:07 +0200

    Add current address to append

--- pypiuma/templatetags/pypiuma_tags.py
+++ pypiuma/templatetags/pypiuma_tags.py
@@ -6,8 +6,16 @@ from django.conf import settings
 register = template.Library()
 
 
-@register.simple_tag
-def piuma(url, width=0, height=0, quality=80):
+def get_host_url(request):
+    if request:
+        return '{0}://{1}'.format(
+            request.scheme, request.META['HTTP_HOST']
+        )
+    return ''
+
+
+@register.simple_tag(takes_context=True)
+def piuma(context, url, width=0, height=0, quality=80):
     if getattr(settings, 'PIUMA_DISABLED', False):
         return url
     piuma_host = getattr(settings, 'PIUMA_HOST', '/piuma/')
@@ -15,4 +23,7 @@ def piuma(url, width=0, height=0, quality=80):
         '{0}_{1}_{2}'.format(width, height, quality),
         url
     ])
-    return '/'.join(i.rstrip('/') for i in [piuma_host, piuma_url])
+    piuma_url = '/'.join(i.rstrip('/') for i in [piuma_host, piuma_url])
+    if not piuma_url.startswith('http'):
+        piuma_url = get_host_url(context['request']) + piuma_url
+    return piuma_url
