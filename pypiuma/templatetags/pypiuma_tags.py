6df975e7f65365a82dc25381445d97fd53529a1b
Andrea Stagi <stagi.andrea@gmail.com>
2019-10-08 00:40:25 +0200

    Add disable piuma setting

--- pypiuma/templatetags/pypiuma_tags.py
+++ pypiuma/templatetags/pypiuma_tags.py
@@ -8,9 +8,11 @@ register = template.Library()
 
 @register.simple_tag
 def piuma(url, width=0, height=0, quality=80):
+    if getattr(settings, 'PIUMA_DISABLED', False):
+        return url
     piuma_host = getattr(settings, 'PIUMA_HOST', '/piuma/')
-    return os.path.join(
-        piuma_host,
+    piuma_url = '/'.join(i.strip('/') for i in [
         '{0}_{1}_{2}'.format(width, height, quality),
         url
-    )
+    ])
+    return '/'.join(i.rstrip('/') for i in [piuma_host, piuma_url])
