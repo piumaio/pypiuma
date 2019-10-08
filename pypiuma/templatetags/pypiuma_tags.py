583b6c067f3e898709d5e87f67304df5a7ccec91
Andrea Stagi <stagi.andrea@gmail.com>
2019-10-08 10:58:06 +0200

    Better url join

--- pypiuma/templatetags/pypiuma_tags.py
+++ pypiuma/templatetags/pypiuma_tags.py
@@ -19,11 +19,12 @@ def piuma(context, url, width=0, height=0, quality=80):
     if getattr(settings, 'PIUMA_DISABLED', False):
         return url
     piuma_host = getattr(settings, 'PIUMA_HOST', '/piuma/')
-    piuma_url = '/'.join(i.strip('/') for i in [
+    if not piuma_host.startswith('http'):
+        piuma_host = get_host_url(context['request']) + piuma_host
+    piuma_url = '{0}/{1}/{2}'.format(
+        piuma_host,
         '{0}_{1}_{2}'.format(width, height, quality),
         url
-    ])
-    piuma_url = '/'.join(i.rstrip('/') for i in [piuma_host, piuma_url])
-    if not piuma_url.startswith('http'):
-        piuma_url = get_host_url(context['request']) + piuma_url
+    )
     return piuma_url
+
