33707b966274c6c90401e2b8902cc7691ec9b7e2
Edoardo Grassi <edo.g@hotmail.it>
2022-05-04 09:57:07 +0200

    chore: black formatting

--- pypiuma/templatetags/pypiuma_tags.py
+++ pypiuma/templatetags/pypiuma_tags.py
@@ -14,50 +14,98 @@ register = template.Library()
 
 def get_host_url(request):
     if request:
-        return '{0}://{1}'.format(
-            request.scheme, request.META['HTTP_HOST']
-        )
-    return ''
+        return "{0}://{1}".format(request.scheme, request.META["HTTP_HOST"])
+    return ""
 
 
 def piuma_size(size, **params):
-    if getattr(settings, 'PIUMA_SIZES', {}).get(size):
+    if getattr(settings, "PIUMA_SIZES", {}).get(size):
         params = {**params, **settings.PIUMA_SIZES[size]}
-    elif not size and "*" in getattr(settings, 'PIUMA_SIZES', {}):
+    elif not size and "*" in getattr(settings, "PIUMA_SIZES", {}):
         params = {**params, **settings.PIUMA_SIZES["*"]}
 
     return params
 
 
 def piuma_media_rules(image_width=None):
-    return getattr(
-        settings,
-        "PIUMA_MEDIA_RULES",
-        "(max-width: 576px),(max-width: 768px),(max-width: 992px),(max-width: 1366px)",
-    ) + (",(max-width: {0}px)".format(image_width) if image_width else "")
-
-
-@register.simple_tag(takes_context=True)
-def piuma(context, image_url, width=0, height=0, quality=100, adaptive_quality=False, convert_to="", size=""):
-    if getattr(settings, 'PIUMA_DISABLED', False) or re.match(exclude_regex, image_url):
-        return image_url
-    if not image_url.startswith('http'):
-        image_url = get_host_url(
-            context.get('request', None)
-        ).rstrip('/') + '/' + image_url.lstrip('/')
-    return piuma_url(
-        getattr(settings, 'PIUMA_HOST', '/piuma/'),
-        image_url, **piuma_size(size, width=width, height=height, quality=quality, adaptive_quality=adaptive_quality, convert_to=convert_to),
+    return (
+        getattr(
+            settings,
+            "PIUMA_MEDIA_RULES",
+            "(max-width: 576px),(max-width: 768px),(max-width: 992px),(max-width: 1366px)",
+        )
+        + (",(max-width: {0}px)".format(image_width) if image_width else "")
     )
 
 
 @register.simple_tag(takes_context=True)
-def piuma_static(context, image_url, width=0, height=0, quality=100, adaptive_quality=False, convert_to="", size=""):
-    return piuma(context, static(image_url), width, height, quality, adaptive_quality, convert_to, size)
+def piuma(
+    context,
+    image_url,
+    width=0,
+    height=0,
+    quality=100,
+    adaptive_quality=False,
+    convert_to="",
+    size="",
+):
+    if getattr(settings, "PIUMA_DISABLED", False) or re.match(exclude_regex, image_url):
+        return image_url
+    if not image_url.startswith("http"):
+        image_url = (
+            get_host_url(context.get("request", None)).rstrip("/")
+            + "/"
+            + image_url.lstrip("/")
+        )
+    return piuma_url(
+        getattr(settings, "PIUMA_HOST", "/piuma/"),
+        image_url,
+        **piuma_size(
+            size,
+            width=width,
+            height=height,
+            quality=quality,
+            adaptive_quality=adaptive_quality,
+            convert_to=convert_to,
+        ),
+    )
 
 
 @register.simple_tag(takes_context=True)
-def piuma_img(context, image_url, width=0, height=0, quality=100, adaptive_quality=False, convert_to="", size="", **img_attributes):
+def piuma_static(
+    context,
+    image_url,
+    width=0,
+    height=0,
+    quality=100,
+    adaptive_quality=False,
+    convert_to="",
+    size="",
+):
+    return piuma(
+        context,
+        static(image_url),
+        width,
+        height,
+        quality,
+        adaptive_quality,
+        convert_to,
+        size,
+    )
+
+
+@register.simple_tag(takes_context=True)
+def piuma_img(
+    context,
+    image_url,
+    width=0,
+    height=0,
+    quality=100,
+    adaptive_quality=False,
+    convert_to="",
+    size="",
+    **img_attributes
+):
     def generate_img(url, attrs):
         return mark_safe(
             "<img src='{0}' {1}>".format(
@@ -82,7 +130,9 @@ def piuma_img(context, image_url, width=0, height=0, quality=100, adaptive_quali
         piuma_media_rules(width or params.get("width", 0) or 0),
         width or params.get("width", 0) or 0,
     )
-    img_attributes["sizes"] = ",".join(["{0} {1}px".format(*media_rule) for media_rule in media_rules])
+    img_attributes["sizes"] = ",".join(
+        ["{0} {1}px".format(*media_rule) for media_rule in media_rules]
+    )
     img_attributes["srcset"] = ",".join(
         [
             "{0} {1}".format(
@@ -101,31 +151,47 @@ def piuma_img(context, image_url, width=0, height=0, quality=100, adaptive_quali
 
 
 @register.simple_tag(takes_context=True)
-def piuma_img_static(context, image_url, width=0, height=0, quality=100, adaptive_quality=False, convert_to="", size="", **img_attributes):
-    return piuma_img(context, static(image_url), width, height, quality, adaptive_quality, convert_to, size, **img_attributes)
+def piuma_img_static(
+    context,
+    image_url,
+    width=0,
+    height=0,
+    quality=100,
+    adaptive_quality=False,
+    convert_to="",
+    size="",
+    **img_attributes
+):
+    return piuma_img(
+        context,
+        static(image_url),
+        width,
+        height,
+        quality,
+        adaptive_quality,
+        convert_to,
+        size,
+        **img_attributes,
+    )
 
 
 def _generate_srcset(context, image_url, media_rule, size, **params):
     return "<source media='{0}' srcset='{1}'>".format(
-        media_rule,
-        piuma(context, image_url, **{**params, "width": size, "height": 0})
+        media_rule, piuma(context, image_url, **{**params, "width": size, "height": 0})
     )
 
 
 def _generate_media_rules_sizes(context, media_rules, size_limit=0):
     generated_media_rules = []
-    media_rules = media_rules.split(',')
+    media_rules = media_rules.split(",")
     for media_rule in media_rules:
-        sanitized_media_rule = media_rule.replace(
-            ' ', ''
-        ).replace(
-            '(', ''
-        ).replace(
-            ')', ''
-        ).replace(
-            'px', ''
+        sanitized_media_rule = (
+            media_rule.replace(" ", "")
+            .replace("(", "")
+            .replace(")", "")
+            .replace("px", "")
         )
-        size = sanitized_media_rule.split(':')[1]
+        size = sanitized_media_rule.split(":")[1]
 
         if int(size_limit) <= 0 or (int(size) <= int(size_limit)):
             generated_media_rules.append((media_rule, size))
@@ -133,27 +199,52 @@ def _generate_media_rules_sizes(context, media_rules, size_limit=0):
 
 
 def _generate_picture_tag(**picture_attributes):
-    return "<picture {0}>".format(" ".join(['{0}="{1}"'.format(k, v) for k, v in picture_attributes.items()]))
+    return "<picture {0}>".format(
+        " ".join(['{0}="{1}"'.format(k, v) for k, v in picture_attributes.items()])
+    )
 
 
 def _generate_picture_img(context, image_url, **img_attributes):
     return "<img src='{0}' {1}>".format(
         piuma(context, image_url),
-        " ".join(['{0}="{1}"'.format(k, v) for k, v in img_attributes.items()])
+        " ".join(['{0}="{1}"'.format(k, v) for k, v in img_attributes.items()]),
     )
 
 
 @register.simple_tag(takes_context=True)
-def piuma_picture(context, image_url, media_rules=None, width=0, height=0, quality=100, adaptive_quality=False, convert_to="", size="", **attributes):
+def piuma_picture(
+    context,
+    image_url,
+    media_rules=None,
+    width=0,
+    height=0,
+    quality=100,
+    adaptive_quality=False,
+    convert_to="",
+    size="",
+    **attributes
+):
     media_rules = media_rules or piuma_media_rules()
-    picture_attributes = {key: value for key, value in attributes.items() if key.startswith('picture_')}
-    img_attributes = {key: value for key, value in attributes.items() if key.startswith('img_')}
-    params = piuma_size(size, width=width, height=height, quality=quality, adaptive_quality=adaptive_quality, convert_to=convert_to)
+    picture_attributes = {
+        key: value for key, value in attributes.items() if key.startswith("picture_")
+    }
+    img_attributes = {
+        key: value for key, value in attributes.items() if key.startswith("img_")
+    }
+    params = piuma_size(
+        size,
+        width=width,
+        height=height,
+        quality=quality,
+        adaptive_quality=adaptive_quality,
+        convert_to=convert_to,
+    )
     html = _generate_picture_tag(**picture_attributes)
-    for media_rule_size in _generate_media_rules_sizes(context, media_rules, width or params.get("width", 0)):
+    for media_rule_size in _generate_media_rules_sizes(
+        context, media_rules, width or params.get("width", 0)
+    ):
         html += _generate_srcset(
-            context, image_url,
-            media_rule_size[0], media_rule_size[1]
+            context, image_url, media_rule_size[0], media_rule_size[1]
         )
     if params.get("width") and params.get("height"):
         img_attributes["width"] = params.get("width")
@@ -164,9 +255,27 @@ def piuma_picture(context, image_url, media_rules=None, width=0, height=0, quali
 
 
 @register.simple_tag(takes_context=True)
-def piuma_picture_static(context, image_url, media_rules=None, width=0, height=0, quality=100, adaptive_quality=False, convert_to="", size="", **attributes):
+def piuma_picture_static(
+    context,
+    image_url,
+    media_rules=None,
+    width=0,
+    height=0,
+    quality=100,
+    adaptive_quality=False,
+    convert_to="",
+    size="",
+    **attributes
+):
     return piuma_picture(
-        context, static(image_url), media_rules,
-        width, height, quality, adaptive_quality,
-        convert_to, size, **attributes
+        context,
+        static(image_url),
+        media_rules,
+        width,
+        height,
+        quality,
+        adaptive_quality,
+        convert_to,
+        size,
+        **attributes,
     )
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,94 +1,86 @@
 from django.test import RequestFactory
 
 from pypiuma import piuma_url
-from pypiuma.templatetags.pypiuma_tags import piuma, piuma_img, piuma_img_static, piuma_static, piuma_picture, piuma_picture_static
+from pypiuma.templatetags.pypiuma_tags import (
+    piuma,
+    piuma_img,
+    piuma_img_static,
+    piuma_static,
+    piuma_picture,
+    piuma_picture_static,
+)
 
 
 def test_piuma_url(settings, client):
-    piumaurl = piuma_url('http://mypiumahost', 'http://myhost/static/img/a.png', 200, 200, 80)
-    assert piumaurl == 'http://mypiumahost/200_200_80/http://myhost/static/img/a.png'
-    piumaurl = piuma_url('http://mypiumahost', '/static/img/a.png', 200, 200, 80)
-    assert piumaurl == 'http://mypiumahost/200_200_80//static/img/a.png'
-    piumaurl = piuma_url('http://mypiumahost', '/static/img/a.png', 200, 200, 80, adaptive_quality=True)
-    assert piumaurl == 'http://mypiumahost/200_200_80a//static/img/a.png'
-    piumaurl = piuma_url('http://mypiumahost', '/static/img/a.png', 200, 200, 80, adaptive_quality=True, convert_to="auto")
-    assert piumaurl == 'http://mypiumahost/200_200_80a:auto//static/img/a.png'
+    piumaurl = piuma_url(
+        "http://mypiumahost", "http://myhost/static/img/a.png", 200, 200, 80
+    )
+    assert piumaurl == "http://mypiumahost/200_200_80/http://myhost/static/img/a.png"
+    piumaurl = piuma_url("http://mypiumahost", "/static/img/a.png", 200, 200, 80)
+    assert piumaurl == "http://mypiumahost/200_200_80//static/img/a.png"
+    piumaurl = piuma_url(
+        "http://mypiumahost", "/static/img/a.png", 200, 200, 80, adaptive_quality=True
+    )
+    assert piumaurl == "http://mypiumahost/200_200_80a//static/img/a.png"
+    piumaurl = piuma_url(
+        "http://mypiumahost",
+        "/static/img/a.png",
+        200,
+        200,
+        80,
+        adaptive_quality=True,
+        convert_to="auto",
+    )
+    assert piumaurl == "http://mypiumahost/200_200_80a:auto//static/img/a.png"
 
 
 def test_piuma_tag_without_request(settings, client):
     context = {}
-    piumaurl = piuma(context, 'img/a.png')
-    assert piumaurl == '/piuma/0_0_100//img/a.png'
+    piumaurl = piuma(context, "img/a.png")
+    assert piumaurl == "/piuma/0_0_100//img/a.png"
     settings.PIUMA_DISABLED = True
-    piumaurl = piuma(context, 'img/a.png')
-    assert piumaurl == 'img/a.png'
+    piumaurl = piuma(context, "img/a.png")
+    assert piumaurl == "img/a.png"
 
 
 def test_piuma_tag(settings, client):
-    context = {
-        'request' : RequestFactory(HTTP_HOST='localhost:8000').get('/')
-    }
-    piumaurl = piuma(context, 'img/a.png')
-    assert piumaurl == '/piuma/0_0_100/http://localhost:8000/img/a.png'
+    context = {"request": RequestFactory(HTTP_HOST="localhost:8000").get("/")}
+    piumaurl = piuma(context, "img/a.png")
+    assert piumaurl == "/piuma/0_0_100/http://localhost:8000/img/a.png"
     settings.PIUMA_DISABLED = True
-    piumaurl = piuma(context, 'img/a.png')
-    assert piumaurl == 'img/a.png'
+    piumaurl = piuma(context, "img/a.png")
+    assert piumaurl == "img/a.png"
 
 
 def test_static_piuma_tag(settings, client):
-    context = {
-        'request' : RequestFactory(HTTP_HOST='localhost:8000').get('/')
-    }
-    piumaurl = piuma_static(context, 'img/a.png')
-    assert piumaurl == '/piuma/0_0_100/http://localhost:8000/static/img/a.png'
+    context = {"request": RequestFactory(HTTP_HOST="localhost:8000").get("/")}
+    piumaurl = piuma_static(context, "img/a.png")
+    assert piumaurl == "/piuma/0_0_100/http://localhost:8000/static/img/a.png"
     settings.PIUMA_DISABLED = True
-    piumaurl = piuma_static(context, 'img/a.png')
-    assert piumaurl == '/static/img/a.png'
+    piumaurl = piuma_static(context, "img/a.png")
+    assert piumaurl == "/static/img/a.png"
 
 
 def test_piuma_picture(settings, client):
-    context = {
-        'request' : RequestFactory(HTTP_HOST='localhost:8000').get('/')
-    }
-    print (
+    context = {"request": RequestFactory(HTTP_HOST="localhost:8000").get("/")}
+    print(
         piuma_picture(
             context,
             "http://localhost:8000/img/a.png",
-            "(max-width: 576px),(max-width: 768px)"
+            "(max-width: 576px),(max-width: 768px)",
         )
     )
 
-    print (
-        piuma_picture(
-            context,
-            "http://localhost:8000/img/a.png"
-        )
-    )
+    print(piuma_picture(context, "http://localhost:8000/img/a.png"))
 
-    print (
-        piuma_picture_static(
-            context,
-            "img/a.png"
-        )
-    )
+    print(piuma_picture_static(context, "img/a.png"))
+
 
 def test_piuma_img(settings, client):
-    context = {
-        'request' : RequestFactory(HTTP_HOST='localhost:8000').get('/')
-    }
+    context = {"request": RequestFactory(HTTP_HOST="localhost:8000").get("/")}
 
-    piuma_img(
-        context,
-        "http://localhost:8000/img/a.png"
-    )
+    piuma_img(context, "http://localhost:8000/img/a.png")
 
-    piuma_img(
-        context,
-        "http://localhost:8000/img/a.png",
-        500
-    )
+    piuma_img(context, "http://localhost:8000/img/a.png", 500)
 
-    piuma_img_static(
-        context,
-        "img/a.png"
-    )
+    piuma_img_static(context, "img/a.png")
