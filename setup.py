f404a0ba67f2039768fb65442e74af4992f5ff0b
Edoardo Grassi <edo.g@hotmail.it>
2022-05-09 17:40:26 +0200

    chore: version 1.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pypiuma',
-    version='1.1.4',
+    version='1.1.5',
     url='https://github.com/piumaio',
     install_requires=[],
     description="Piuma Python library with Django support",
